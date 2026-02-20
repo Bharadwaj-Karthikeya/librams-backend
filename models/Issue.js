96dbab263b2e87147d444d0849d156051dc4c996
Bharadwaj Karthikeya <pbk311205@gmail.com>
2026-02-20 20:38:04 +0530

    feature: Issue data model created

---
+++ models/Issue.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const issueSchema = new mongoose.Schema({
+    book: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Book",
+        required: true,
+        index: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true,
+    },
+    issueDate: {
+        type: Date,
+        default: Date.now,
+    },
+    dueDate: {
+        type: Date,
+        required: true,
+        index: true,
+    },
+    status: {
+        type: String,
+        enum: ["issued", "returned", "overdue"],
+        default: "issued",
+        index: true,
+    },
+}, { timestamps: true });
+
+const Issue = mongoose.model("Issue", issueSchema);
+
+export default Issue;
