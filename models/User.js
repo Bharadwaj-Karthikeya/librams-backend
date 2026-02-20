97643c02f40f882e71377a3c81a227e0e958fc30
Bharadwaj Karthikeya <pbk311205@gmail.com>
2026-02-20 20:34:03 +0530

    feature: User data model added

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+    type: String,
+    required: true,
+    },
+    email: {    
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+    },
+    password: {
+    type: String,
+    required: true,
+    },
+    role: {
+    type: String,
+    enum: ["admin", "staff", "student"],
+    default: 'student',
+    index: true,
+    },
+    
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
